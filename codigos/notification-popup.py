1fdfdc53efb03460bab865d3a313c0f5fdc5db14
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2017-03-08 23:17:59 -0300

    refatorando apendice a

--- codigos/notification-popup.py
+++ codigos/notification-popup.py
@@ -1,12 +1,3 @@
-def showNotificationOk(self, id, header, message):
-    try:
-      header = header.encode('utf-8')
-      message = message.encode('utf-8')
-      if XBMC_ENABLE == True:
-        xbmcgui.Dialog().ok(header, message)
-    except Exception, e:
-      logging.error("Erro -> notificacao OK ID: %s : %s" % (id, e))
-
 def showNotificationPopup(self, id, header, message):
   try:
     header = header.encode('utf-8')
